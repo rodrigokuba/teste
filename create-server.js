21bed6a82331823adaa998c0b75e07378c488a2b
EC2 Default User <ec2-user@ip-172-31-18-98.us-west-2.compute.internal>
2016-04-19 06:29:05 +0000

    Simple server functionality added

---
+++ create-server.js
@@ -0,0 +1,14 @@
+var server = http.createServer(function(request, response) {
+  var filePath = false;
+
+  if (request.url == '/') {
+    filePath = "public/index.html";
+  } else {
+    filePath = "public" + request.url;
+  }
+
+  var absPath = "./" + filePath;
+  serverWorking(response, absPath);
+});
+
+var port_number = server.listen(process.env.PORT || 3000);
