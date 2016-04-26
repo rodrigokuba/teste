246e70cfde4ad6c738680172abc8f4735bcd63a8
EC2 Default User <ec2-user@ip-172-31-18-98.us-west-2.compute.internal>
2016-04-26 23:49:50 +0000

    test3

--- script/heroku.sh
+++ script/heroku.sh
@@ -1,3 +1,20 @@
 #!/bin/sh
 
-git push -u heroku master 
+#git push -u heroku master 
+
+# exit 1 on errors
+set -e
+
+# deal with remote
+echo "Checking if remote exists..."
+
+if ! git ls-remote heroku; then
+  echo "Adding heroku remote..."
+  git remote add heroku git@heroku.com:infinite-sea-32351.git
+fi
+
+# push only origin/master to heroku/master - will do nothing if
+# master doesn't change.
+echo "Updating heroku master branch..."
+git push heroku origin/master:master
+
